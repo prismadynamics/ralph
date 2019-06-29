26a948e9f97ab7ad69b6b4cb3d66922fe77f9b32
Ao Yu <engietoday@gmail.com>
2019-06-29 15:32:53 -0400

    Initial Commit

--- docs/ralph_setup/setup_github.sh
+++ docs/ralph_setup/setup_github.sh
@@ -16,13 +16,39 @@ cd $gitdirectroy
 if [ ! -d "ralph" ]; then
 	git clone $github_url
 else
-	echo "INFO: Ralph repo detected in $gitdirectroy. Skipping clone."
+	echo "INFO: ralph repo detected in $gitdirectroy. Skipping clone."
 	echo "INFO: This script is intended for first time setup."
 fi
 cd ralph
 git fetch --all
 git pull
 git config --global credential.helper wincred
+
+# git clone opencv
+opencv_version="4.1.0"
+cd gitdirectroy
+if [ ! -d "opencv" ]; then
+	git clone https://github.com/opencv/opencv.git
+else
+	echo "INFO: opencv repo detected in $gitdirectroy. Skipping clone."
+fi
+if [ ! -d "opencv_contrib" ]; then
+	git clone https://github.com/opencv/opencv_contrib.git
+else
+	echo "INFO: opencv_contribnCV repo detected in $gitdirectroy. Skipping clone."
+fi
+cd opencv
+if [$pwd -eq "$HOME/githome/opencv"]; then
+	git fetch --all
+	git checkout $opencv_version
+fi
+cd gitdirectroy
+cd opencv_contrib
+if [$pwd -eq "$HOME/githome/opencv"]; then
+	git fetch --all
+	git checkout $opencv_version
+fi
+cd gitdirectroy
 
 
 
